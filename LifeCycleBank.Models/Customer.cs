fbf2ff3388cfe30710ea73967e88e39f3505e76e
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-01 11:57:16 +0100

    Changes made to Customer: add more properties.

--- LifeCycleBank.Models/Customer.cs
+++ LifeCycleBank.Models/Customer.cs
@@ -5,10 +5,13 @@ namespace LifeCycleBank.Models
     public class Customer
     {
         public int Id { get; set; }
-        public string CustomerNumber { get; set; }
         public string OrganizationNumber { get; set; }
+        public string CompanyName { get; set; }
         public string Address { get; set; }
         public string PostalCode { get; set; }
         public string City { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
