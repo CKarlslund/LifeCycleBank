94e0d1f11741e820702eccfd654832921355a263
mariakallstrom <mariakallstrom@outlook.com>
2017-11-01 12:51:33 +0100

    Change in ReadFileData Class

--- LifeCycleBank/ReadFileData.cs
+++ LifeCycleBank/ReadFileData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace LifeCycleBank
 {
@@ -9,14 +10,19 @@ namespace LifeCycleBank
     {
         public static void ReadFileFromBankData()
         {
+            var amount = new List<int>();
             var path = Path.Combine(Directory.GetCurrentDirectory(), "bankdata\\bankdata-small.txt");
             string[] lines = System.IO.File.ReadAllLines(path);
             foreach (var line in lines)
             {
-                Console.WriteLine(line);
+               
+
+                
             }
           
             Console.ReadLine();
         }
+
+     
     }
 }
