65d132f7fe0ffd857fc02ee6ed021caad1943f9d
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-01 11:52:01 +0100

    Add class: Customer

---
+++ LifeCycleBank.Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LifeCycleBank.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public string OrganizationNumber { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
