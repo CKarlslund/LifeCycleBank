efc6ee4e91505731c11374a0a5629a3ca2486a87
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-01 10:35:50 +0100

    Add project files.

---
+++ LifeCycleBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LifeCycleBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LifeCycleBank!");
+        }
+    }
+}
