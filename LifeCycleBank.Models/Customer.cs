7a6d9a98d8c00d217a6965f34e8d3e08a97577c2
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-01 11:54:20 +0100

    Changed AccountNumber to CustomerNumber.

--- LifeCycleBank.Models/Customer.cs
+++ LifeCycleBank.Models/Customer.cs
@@ -5,7 +5,7 @@ namespace LifeCycleBank.Models
     public class Customer
     {
         public int Id { get; set; }
-        public string AccountNumber { get; set; }
+        public string CustomerNumber { get; set; }
         public string OrganizationNumber { get; set; }
         public string Address { get; set; }
         public string PostalCode { get; set; }
