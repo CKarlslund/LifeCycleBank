a7b83cccec0e244506e6bf69373473aa41e15fbc
annasandberg <Anna.Kallman@yh.nackademin.se>
2017-11-02 16:22:16 +0100

    Print statistics when new file is created

--- LifeCycleBank/Program.cs
+++ LifeCycleBank/Program.cs
@@ -13,5 +13,18 @@ namespace LifeCycleBank
            
             Console.ReadLine();
         }
+
+        private static void CreateFileAndDisplayStatistics()
+        {
+            var fileName = CreateFileData.CreateFile();
+            var statistics = ReadStatisticFromBankData.GetStatistics();
+
+            Console.WriteLine("Sparar till " + "fileName" + "...");
+            Console.WriteLine("Antal kunder: " + statistics["numberOfCustomers"]);
+            Console.WriteLine("Antal konton: " + statistics["numberOfAccounts"]);
+            Console.WriteLine("Totalt saldo: " + statistics["totalBalance"]);
+            Console.WriteLine("Totalt saldo: " + statistics["totalBalance"]);
+            Console.ReadLine();
+        }
     }
 }
