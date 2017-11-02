7a12e78a3d546711e196cf9ed688cb76fb244efb
annasandberg <Anna.Kallman@yh.nackademin.se>
2017-11-02 13:32:21 +0100

    Add public keyword to models

--- LifeCycleBank.Models/Bank.cs
+++ LifeCycleBank.Models/Bank.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace LifeCycleBank.Models
 {
-    class Bank
+    public class Bank
     {
         public int Id { get; set; }
         public List<Account> Accounts { get; set; }
