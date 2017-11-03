960815371a5c49e0ad45afc8284c6e1716ccd9bb
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-03 11:21:26 +0100

    Add project: Interfaces

---
+++ LifeCycleBank.Interfaces/ICustomer.cs
@@ -0,0 +1,15 @@
+﻿namespace LifeCycleBank.Interfaces
+{
+    public interface ICustomer
+    {
+        int Id { get; set; }
+        string OrganizationNumber { get; set; }
+        string CompanyName { get; set; }
+        string Address { get; set; }
+        string PostalCode { get; set; }
+        string City { get; set; }
+        string Region { get; set; }
+        string Country { get; set; }
+        string PhoneNumber { get; set; }
+    }
+}
