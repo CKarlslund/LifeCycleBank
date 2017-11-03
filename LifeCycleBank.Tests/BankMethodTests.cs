2d2ebc71580b70b902e4f328f7b8322a55311708
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-03 14:54:57 +0100

    Add tests for transaction methods.

--- LifeCycleBank.Tests/BankMethodTests.cs
+++ LifeCycleBank.Tests/BankMethodTests.cs
@@ -34,29 +34,43 @@ namespace LifeCycleBank.Tests
         {
             var account = _bank.Accounts.FirstOrDefault(a => a.Id == 13003);
 
-            _bank.CreateDeposit(account, 700.00M);
+            _bank.CreateWithdrawal(account, 200.00M);
 
             var result = _bank.Accounts.FirstOrDefault(a => a.Id == account.Id);
 
-            Assert.Equal(1000.00M, result.Balance);
+            Assert.Equal(100.00M, result.Balance);
         }
 
         [Fact]
         public void Cannot_Withdraw_More_Than_Balance()
         {
-            //TODO tests
+            var account = _bank.Accounts.FirstOrDefault(a => a.Id == 13003);
+
+            Assert.Throws<AccountBalanceException>(() => _bank.CreateWithdrawal(account, 500.00M));
         }
 
         [Fact]
         public void Can_Create_Transaction()
         {
-            //TODO tests
+            var fromAccount = _bank.Accounts.FirstOrDefault(a => a.Id == 13003);
+            var toAccount = _bank.Accounts.FirstOrDefault(a => a.Id == 13004);
+
+            _bank.CreateTransaction(fromAccount, toAccount, 200);
+
+            var fromAccountResult = _bank.Accounts.FirstOrDefault(a => a.Id == fromAccount.Id);
+            var toAccountResult = _bank.Accounts.FirstOrDefault(a => a.Id == toAccount.Id);
+
+            Assert.Equal(100, fromAccountResult.Balance);
+            Assert.Equal(600, toAccountResult.Balance);
         }
 
         [Fact]
         public void Cannot_Transact_More_Than_Balance()
         {
-            //TODO tests
+            var fromAccount = _bank.Accounts.FirstOrDefault(a => a.Id == 13003);
+            var toAccount = _bank.Accounts.FirstOrDefault(a => a.Id == 13004);
+
+            Assert.Throws<AccountBalanceException>(() => _bank.CreateTransaction(fromAccount, toAccount, 400));
         }
     }
 }
