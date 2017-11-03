1c3e020e5dfe48ce068b0173ed68985524052447
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-03 14:54:12 +0100

    Add custom Exception for transaction error.

---
+++ LifeCycleBank/AccountBalanceException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeCycleBank
+{
+    public class AccountBalanceException : Exception
+    {
+        public AccountBalanceException()
+        {
+            
+        }
+
+        public AccountBalanceException(string message) : base(message)
+        {
+            
+        }
+
+        public AccountBalanceException(string message, Exception inner) : base(message, inner)
+        {
+            
+        }
+    }
+}
