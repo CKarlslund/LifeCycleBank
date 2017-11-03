85293ebddc1cfa846057e35c53fcd4674002b19d
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-03 11:31:21 +0100

    Add Interface to Bank.cs

--- LifeCycleBank.Models/Bank.cs
+++ LifeCycleBank.Models/Bank.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using LifeCycleBank.Interfaces;
 
 namespace LifeCycleBank.Models
 {
-    public class Bank
+    public class Bank : IBank
     {
+        private static Bank _instance;
+
+        public static Bank Instance
+        {
+            get
+            {
+                if (_instance == null)
+                _instance = new Bank();
+                return _instance;
+            }
+        }
+
         public int Id { get; set; }
-        public List<Account> Accounts { get; set; }
-        public List<Customer> Customers { get; set; }
-        public List<Transaction> Type { get; set; }
+        public List<IAccount> Accounts { get; set; }
+        public List<ICustomer> Customers { get; set; }
+
+        public void GetBankData()
+        {
+            //Read all the bank data
+        }
     }
 }
