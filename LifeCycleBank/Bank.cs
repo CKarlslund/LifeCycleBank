a41c8412ec38711b2a82d0be232c9a0ae9c51bb7
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-11-03 14:53:20 +0100

    Add methods for transaction.

--- LifeCycleBank/Bank.cs
+++ LifeCycleBank/Bank.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using LifeCycleBank.Interfaces;
 using System.Collections.Generic;
-using System.Text;
-using LifeCycleBank.Interfaces;
+using System.Linq;
 
-namespace LifeCycleBank.Models
+namespace LifeCycleBank
 {
     public class Bank : IBank
     {
@@ -31,6 +30,44 @@ namespace LifeCycleBank.Models
         {
             Accounts = ReadFileData.GetAllAccounts();
             Customers = ReadFileData.GetAllCustomers();
+        }
+
+        public void CreateDeposit(IAccount toAccount, decimal amount)
+        {
+            var account = Accounts.FirstOrDefault(a => a.Id == toAccount.Id);
+
+            account.Balance += amount;
+        }
+
+        public void CreateWithdrawal(IAccount fromAccount, decimal amount)
+        {
+            var account = Accounts.FirstOrDefault(a => a.Id == fromAccount.Id);
+
+            if (amount <= account.Balance)
+            {
+                account.Balance = account.Balance - amount;
+            }
+            else
+            {
+                throw new AccountBalanceException("The targeted account does not have enough money!");
+            }
+        }
+
+        public void CreateTransaction(IAccount fromAccount, IAccount toAccount, decimal amount)
+        {
+            var creditAccount = Accounts.FirstOrDefault(a => a.Id == fromAccount.Id);
+            var debitAccount = Accounts.FirstOrDefault(a => a.Id == toAccount.Id);
+
+
+            if (amount <= creditAccount.Balance)
+            {
+                creditAccount.Balance = creditAccount.Balance - amount;
+                debitAccount.Balance += amount;
+            }
+            else
+            {
+                throw new AccountBalanceException("The specified amount was bigger than the available sum on the credit account. Could not continue.");
+            }
         }
     }
 }
