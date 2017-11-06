d8d154c0fe0295db328bf2479afe9e9093fc03f7
mariakallstrom <mariakallstrom@outlook.com>
2017-11-06 13:05:32 +0100

    Test success

---
+++ LifeCycleBank.Tests/BankDataTests.cs
@@ -0,0 +1,26 @@
+using LifeCycleBank.services;
+using System;
+using System.IO;
+using Xunit;
+
+namespace LifeCycleBank.Tests
+{
+    public class BankDataTests
+    {
+        [Fact]
+        public void Get_Date_From_FileName_Test()
+        {
+            var fileDate = GetLatestFile.GetDateFromFileName("20171019-1243.txt");
+            var date = new DateTime(2017,10,19,12,43,0);
+            Assert.Equal(date, fileDate);
+        }
+
+        [Fact]
+
+        public void Get_Latest_File_Path_Test()
+        {
+            var path = GetLatestFile.GetPathToLatestFile();
+      
+        }
+    }
+}
