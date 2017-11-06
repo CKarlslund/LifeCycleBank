9cacdadecb005cc2e33bd742a8c3e9d1f042ca9f
annasandberg <Anna.Kallman@yh.nackademin.se>
2017-11-06 10:33:05 +0100

    Update CreateFileData to use IWriter instead of streamwriter

--- LifeCycleBank/services/CreateFileData.cs
+++ LifeCycleBank/services/CreateFileData.cs
@@ -9,28 +9,34 @@ namespace LifeCycleBank.services
 {
     public class CreateFileData
     {
-        public static string CreateFile(IBank bank)
+        private IBank _bank;
+        private IWriter _writer;
+
+        public CreateFileData(IBank bank, IWriter writer)
         {
-            string fileName = DateTime.Now.ToString("yyyyMMdd-HHmm") + ".txt";
-            var path = @"bankdata\";
-            string pathString = Path.Combine(path, fileName);
-
-            using (StreamWriter sw = new StreamWriter(pathString, true))
-            {
-                sw.WriteLine(bank.Customers.Count);
-                foreach (var c in bank.Customers)
-                {
-                    sw.WriteLine($"{0};{1};{2};{3};{4};{5};{6};{7};{8}", c.Id.ToString(), c.OrganizationNumber, c.CompanyName,
-                        c.Address, c.City, c.Region, c.PostalCode, c.Country, c.PhoneNumber);
-                }
-                sw.WriteLine(bank.Accounts.Count);
-                foreach (var a in bank.Accounts)
-                {
-                    sw.WriteLine($"{0};{1};{2}", a.Id.ToString(), a.Owner.Id.ToString(), a.Balance.ToString());
-                }
-            }
-
-            return fileName;
+            _bank = bank;
+            _writer = writer;
         }
+
+        public void CreateFile()
+        {  
+                _writer.WriteLine(_bank.Customers.Count.ToString());
+                foreach (var c in _bank.Customers)
+                {
+                    var customerInfo = string.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8}", c.Id.ToString(), c.OrganizationNumber, c.CompanyName,
+                        c.Address, c.City, c.Region, c.PostalCode, c.Country, c.PhoneNumber).ToString();
+                    _writer.WriteLine(customerInfo);
+                }
+                _writer.WriteLine(_bank.Accounts.Count.ToString());
+                foreach (var a in _bank.Accounts)
+                {
+                    var accountInfo = string.Format("{0};{1};{2}", a.Id.ToString(), a.Owner.Id.ToString(), a.Balance.ToString()).ToString();
+                    _writer.WriteLine(accountInfo);
+                }
+        
+            _writer.Dispose();
+        }
+
     }
+
 }
