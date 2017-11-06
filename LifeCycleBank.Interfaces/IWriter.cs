9762e08aba49699dd059dd61cf12cc41f9fb3eb7
annasandberg <Anna.Kallman@yh.nackademin.se>
2017-11-06 10:32:17 +0100

    Add Writer and interface

---
+++ LifeCycleBank.Interfaces/IWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeCycleBank.Interfaces
+{
+    public interface IWriter : IDisposable
+    {
+        void WriteLine(string value);
+    }
+}
