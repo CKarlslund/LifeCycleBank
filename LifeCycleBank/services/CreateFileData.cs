54d582c9f5eb1706b8d35bd522af110bfe6a8f15
annasandberg <Anna.Kallman@yh.nackademin.se>
2017-11-08 11:26:07 +0100

    Add method GetPath to CreateFileData service, Update CreateFile method to take IWriter as parameter

--- LifeCycleBank/services/CreateFileData.cs
+++ LifeCycleBank/services/CreateFileData.cs
@@ -10,31 +10,36 @@ namespace LifeCycleBank.services
     public class CreateFileData
     {
         private IBank _bank;
-        private IWriter _writer;
-
-        public CreateFileData(IBank bank, IWriter writer)
+        
+        public CreateFileData(IBank bank)
         {
             _bank = bank;
-            _writer = writer;
         }
 
-        public void CreateFile()
+        public string GetPath(string fileName)
+        {
+            var path = @"bankdata\";
+
+            return Path.Combine(path, fileName);
+        }
+
+        public void CreateFile(IWriter writer)
         {  
-                _writer.WriteLine(_bank.Customers.Count.ToString());
+                writer.WriteLine(_bank.Customers.Count.ToString());
                 foreach (var c in _bank.Customers)
                 {
                     var customerInfo = string.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8}", c.Id.ToString(), c.OrganizationNumber, c.CompanyName,
                         c.Address, c.City, c.Region, c.PostalCode, c.Country, c.PhoneNumber).ToString();
-                    _writer.WriteLine(customerInfo);
+                    writer.WriteLine(customerInfo);
                 }
-                _writer.WriteLine(_bank.Accounts.Count.ToString());
+                writer.WriteLine(_bank.Accounts.Count.ToString());
                 foreach (var a in _bank.Accounts)
                 {
                     var accountInfo = string.Format("{0};{1};{2}", a.Id.ToString(), a.Owner.Id.ToString(), a.Balance.ToString()).ToString();
-                    _writer.WriteLine(accountInfo);
+                    writer.WriteLine(accountInfo);
                 }
         
-            _writer.Dispose();
+            writer.Dispose();
         }
 
     }
