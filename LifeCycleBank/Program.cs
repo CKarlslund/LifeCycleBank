45a4af0d895e453e1bd433c409244064f22aa52f
sTaMiNa01 <Matilda.Lind@yh.nackademin.se>
2017-11-14 19:56:07 +0100

    Removed duplicate method after rebase conflict merge

--- LifeCycleBank/Program.cs
+++ LifeCycleBank/Program.cs
@@ -201,17 +201,6 @@ namespace LifeCycleBank
         
         }
 
-        private static void DisplayBankData()
-        {
-            var statistics = ReadStatisticFromBankData.GetStatistics();
-
-            Console.WriteLine("Sparar till " + "Läser in bankdata.txt..." + "...");
-            Console.WriteLine("Antal kunder: " + statistics["numberOfCustomers"]);
-            Console.WriteLine("Antal konton: " + statistics["numberOfAccounts"]);
-            Console.WriteLine("Totalt saldo: " + statistics["totalBalance"] + "kr");
-        
-        }
-
         private static int DisplayMenu()
         {
             Console.WriteLine("*****************************");
